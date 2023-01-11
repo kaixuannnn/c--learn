a14e0797d049b168a07f0d6047d08f17c2383e55
tankaixuan <tankaixuan@outlook.com>
2023-01-11 14:55:56 +0800

    class introduction

---
+++ Classes/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Classes
+{
+    public class Person
+    {
+        // field
+        public string Name;
+
+        // method
+        public void Introduce(string to)
+        {
+            Console.WriteLine("Hi {0}, I am {1}.", to, Name);
+        }
+
+        // static method
+        public static Person Parse(string str)
+        {
+            var person = new Person();
+            person.Name = str;
+
+            return person;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var person = Person.Parse("Kai Xuan");
+            person.Introduce("Tan");
+        }
+    }
+}
